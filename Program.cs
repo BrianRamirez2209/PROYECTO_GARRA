2bb6ce739fc43c9596ce77900cc922f1e77c6458
Brian Ramirez Valdez <22460761@colima.tecnm.mx>
2025-07-07 08:44:35 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROYECTO_GARRA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
