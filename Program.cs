bbdd069397a8e5bd2db63e68547ee03eb787d4da
Brian Ramirez Valdez <22460761@colima.tecnm.mx>
2025-07-07 10:42:21 -0600

    Agregue public Maquina

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 ﻿namespace PROYECTO_GARRA
 {
+    public partial class Maquina
+    {
+        private Sensor sensor;
+        private Motor motor;
+        private Interfaz interfaz;
+        private Garra garra;
 
+
+        public Maquina()
+        {
+            sensor = new Sensor();
+            motor = new Motor();
+            interfaz = new Interfaz();
+            garra = new Garra();
+        }
+
+    }
 
     internal class Program
     {
