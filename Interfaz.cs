9c80040cb36f6f83c371115d524a9218cf4c8fb4
DANY Zurita <danyzurita2004@outlook.com>
2025-07-08 13:24:53 -0600

    codigo para que cierre de garra y movimiento a nuevas coordenadas

--- Interfaz.cs
+++ Interfaz.cs
@@ -16,6 +16,15 @@ using System
 
             Console.WriteLine($"→ Moviendo a ({x1}, {y1}, {z1})");
 
+        if (Confirmar("¿Cerrar garra?"))
+        {
+            Console.WriteLine("→ Garra cerrada.");
+            Console.WriteLine("Coordenadas destino:");
+            float x2 = PedirValor("X");
+            float y2 = PedirValor("Y");
+            float z2 = PedirValor("Z");
+
+            Console.WriteLine($"→ Moviendo a ({x2}, {y2}, {z2})");
         }
     }
 
