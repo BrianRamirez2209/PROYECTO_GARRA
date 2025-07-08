b1ac816289c229b37799742e6ad4d945627a1d13
Brian Ramirez Valdez <22460761@colima.tecnm.mx>
2025-07-08 16:14:59 -0600

    Declare las variables Velocidad, direccion y encendido

--- Motor.cs
+++ Motor.cs
@@ -5,6 +5,14 @@ namespace PROYECTO_GARRA
 {
     public class Motor
     {
+        public float Velocidad;
+        public string Direccion;
+        public bool Encendido;
 
+        public void ActivarMotor()
+        {
+            Encendido = true;
+            Console.WriteLine("Motor activado y girando...");
+        }
     }
 }
