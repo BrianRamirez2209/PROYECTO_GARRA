e1663ed947befb799303f999382a012fca633785
Brian Ramirez Valdez <22460761@colima.tecnm.mx>
2025-07-08 19:16:40 -0600

    Agregue codigo al main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,56 @@
 ﻿namespace PROYECTO_GARRA
 {
+
+    public class Maquina
+    {
+        public Sensor sensor;
+        public Motor motor;
+        public Interfaz interfaz;
+        public Garra garra;
+
+
+        public Maquina()
+        {
+            sensor = new Sensor();
+            motor = new Motor();
+            interfaz = new Interfaz();
+            garra = new Garra();
+        }
+    }
+
+
+    public void InsertarMoneda()
+        {
+            Console.WriteLine("Moneda insertada");
+            interfaz.IniciarJuego();
+
+
+            motor.ActivarMotor();
+            Console.WriteLine("Motor activado");
+
+
+            garra.MoverEjes();
+            garra.ActivarGarra();
+
+            if (sensor.DetectarObjeto())
+            {
+                Console.WriteLine("¡Objeto detectado por la garra!");
+            }
+            else
+            {
+                Console.WriteLine("No se detectó ningún objeto.");
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
